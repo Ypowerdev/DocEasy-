45880936189772db9a421f719907c581274d689d
Ypowerdev <117303538+Ypowerdev@users.noreply.github.com>
2023-02-09 20:06:19 +0300

    Update consumer-protection-claim.php

--- doc/consumer-protection-claim.php
+++ doc/consumer-protection-claim.php
@@ -1,63 +1,61 @@
 <?php 
-if ($_POST) { 
-    
-    foreach($_POST as $fieldName => $fieldValue) {
-        $$fieldName = $fieldValue;
-    }
-}
-?> 
 
 
-<p align='right' style ="padding-top:30px">
-В <?=$companyName;?><br> 
-Адрес:<?=$companyAdress;?>  <br>
-    
-от  <?=$claimantName;?> <br>       
-Адрес: <?=$claimantAdress;?><br>   
-<br>
-     
-<h4 align='center'> Требование о принятии товара ненадлежащего качества и возврате его стоимости, 
-компенсации убытков </h4> <br>
-
-<p> <?= $date."\n"; ?><?=$month."\n"; ?><?= $year."\n"; ?> г.  мной, в магазине <?=$shopName."\n"; ?> , был приобретён смартфон <?=$phoneName."\n"; ?>  цвет <?=$phoneColor."\n";?> imei1:<?=$imei."\n";?> ,  что подтверждается 
-наличием чека (Приложение № 1).</p>
-
-<p>За указанный телефон мной было уплачено <?= $phonePrice."\n"; ?>  рублей.</p>
-<p>Также для совестного использования с указанным смартфоном были приобретены дополнительно: <?= (isset($accesories1)) ? $accesories1  : "  "."\n"; ?> стоимостью <?= (isset($priceAcessories1)) ? $priceAcessories1 : "  " ."\n"; ?> рублей, <?= (isset($accesories2)) ? $accesories2 : "  "."\n";?> стоимостью <?= (isset($priceAcessories2)) ? $priceAcessories2 : "  "."\n"; ?>  рублей. . 
-Гарантийный срок, установленный изготовителем на товар, составляет <?= $warrantyTerm; ?> месяцев. </p>
-<p>В период гарантийного срока  <?= $flawDescription ?>. </p> 
-<p>Так как устройство мной эксплуатировалось в соответствии с инструкцией по эксплуатации, считаю, что причина возникновения недостатков носит производственный характер (недостаток возник до момента передачи или по причинам, возникшим до этого момента). </p>
-<p>На основании вышеизложенного и в соответствии со ст. 18 Закона РФ «О защите прав потребителей» я требую принять товар с недостатками (разъяснить порядок передачи и приёмки его у меня, как у потребителя) и возвратить мне: </p>
-
-<p>1.	Сумму в размере <?= $phonePrice."\n"; ?> рублей (стоимость смартфона  <?=$phoneName."\n";?>  imei: <?=$imei."\n";?>)</p> 
-
-<?php 
 if ($_SERVER["REQUEST_METHOD"] == "POST"){ 
-    if (!empty($accesories1) && !empty($priceAcessories1) && !empty($accesories2) && !empty($priceAcessories2)){ 
-      $totalPrice = $priceAcessories1+$priceAcessories2; 
-          echo "<p>2. Сумму в размере" . "&nbsp" . $totalPrice . "рублей (стоимость дополнительных аксессуаров)."; 
-            }elseif(!empty($accesories1) && !empty($priceAcessories1)){ 
-                echo "<p>2. Сумму в размере". "&nbsp" . $priceAcessories1 . "рублей (стоимость дополнительных аксессуаров)." ; 
-                   } else {
-                       print "   "; 
+
+    $search = [];
+    $replace = [];
+
+    foreach($_POST as $fieldName => $fieldValue) {
+       $$fieldName = $fieldValue;          
+       $search[] = "{{$fieldName}}";
+       $replace[] = $fieldValue;
     }
+    
+
+    if(!empty($accesoriesArrName[0] && !empty($accesoriesArrName[1]))){ 
+        $firstSentence = "<p>Также для совестного использования с указанным смартфоном были приобретены дополнительно:" . "&nbsp;". mb_strtolower($accesoriesArrName[0]) . "стоимостью" . "&nbsp;". mb_strtolower($accesoriesArrPrice[0]) ."&nbsp;". "рублей  и" ."&nbsp;". mb_strtolower($accesoriesArrName[1]) . "&nbsp;" ."стоимостью" . "&nbsp;" . mb_strtolower($accesoriesArrPrice[1]) . "&nbsp;" . "рублей.</p>";      
+    }elseif(!empty($accesoriesArrName[0])){ 
+        $secondSentence = "<p>Также для совестного использования с указанным смартфоном были приобретены дополнительно:" . "&nbsp;" . mb_strtolower($accesoriesArrName[0]) . "&nbsp;". "стоимостью" . "&nbsp;" . mb_strtolower($accesoriesArrPrice[0]) . "&nbsp;" . "рублей.</p>"; 
+    }elseif(!empty($accesoriesArrName[1])){ 
+        $thirdSentence =  "<p>Также для совестного использования с указанным смартфоном были приобретены дополнительно:" . "&nbsp;" . mb_strtolower($accesoriesArrName[1]) . "&nbsp;" . "стоимостью" . "&nbsp;" . mb_strtolower($accesoriesArrPrice[1]) . "&nbsp;". "рублей.</p>"; 
+    }else{ 
+        echo ""; 
+    }
+    
+    $search[] = '{{firstSentence}}'; 
+    $search[] = '{{secondSentence}}'; 
+    $search[] = '{{thirdSentence}}'; 
+    $replace[] = $firstSentence; 
+    $replace[] = $secondSentence; 
+    $replace[] = $thirdSentence; 
+
+    $accesoriesPrice = 0;
+
+    foreach ($accesoriesArrPrice as $key => $value){
+       $accesoriesPrice += $accesoriesArrPrice[$key]; 
+    }     
+    
+    
+    $accesoriesHolder = "<p>2. Сумму в размере" . "&nbsp;" . $accesoriesPrice . "&nbsp;" . "рублей (стоимость дополнительных аксессуаров).";
+
+    $search[] = '{{accesoriesHolder}}';
+    $replace[] =  $accesoriesHolder;
+
+    $totalPrice = $phonePrice + $accesoriesPrice;
+    $totalHolder = "<p>А всего:" . "&nbsp;". $totalPrice ."&nbsp;". "рублей.</p>";
+
+    $search[] = '{{totalHolder}}';
+    $replace[] =  $totalHolder;
+
+    //получаем текст из файла, по названию файла
+    $textTemplate = file_get_contents('template-requirements.html');
+
+    // находим шаблоны вида {{param}} и заменяем на актуальное содержимое
+    $textReplaced = str_replace($search, $replace, $textTemplate);
+
+    // выводим результат
+    echo $textReplaced;
 } 
 
-if ($totalPrice): 
-    $total1 = $phonePrice+$totalPrice; 
-    echo "<p>А всего:" . "&nbsp". $total1 ."&nbsp". "рублей.</p>";  
-elseif ($priceAcessories1):
-    $total2 = $phonePrice+$priceAcessories1; 
-    echo "<p>А всего:". "&nbsp". $total2. "&nbsp". "рублей.</p>";
-else: 
-    echo "<p>А всего:" . "&nbsp". $phonePrice."&nbsp". "рублей.</p>"; 
-endif;
 ?>
-
-<p>Данное требование прошу рассмотреть и удовлетворить в течение 10 дней с момента его получения.</p> 
-
-<p align = 'left'>Приложения: </p>
-
-<p align = 'left'>1.	Документы о продаже от <?= $docDate."\n"; ?><?= $docMonth."\n"; ?><?= $docYear."\n"; ?> г.; </p><br>
-
-<p>Дата:  &nbsp <?= $claimDate."\n"; ?><?= $claimMonth."\n"; ?><?= $claimYear."\n"; ?> г.   &nbsp &nbsp &nbsp &nbsp	 <?=$claimantName; ?>   &nbsp &nbsp  &nbsp &nbsp   Подпись: _________________</p>
