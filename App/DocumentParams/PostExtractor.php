360a8553372ba569860aa6290ea6cdac21115b18
Ypowerdev <117303538+Ypowerdev@users.noreply.github.com>
2023-05-17 19:21:25 +0300

    Add files via upload

---
+++ App/DocumentParams/PostExtractor.php
@@ -0,0 +1,27 @@
+<?php 
+
+namespace App\DocumentParams; 
+
+use App\Exception\CustomException; 
+
+class PostExtractor 
+{ 
+    public function exctractFields($simpleFieldsNames,$postParams): array
+    {        
+        $errorFields = []; 
+        $simpleFields = []; 
+
+        foreach($simpleFieldsNames as $simpleFieldName){
+            if (!isset($postParams[$simpleFieldName])){ 
+                $errorFields[] = $simpleFieldName; 
+            } 
+            $simpleFields[$simpleFieldName] = $postParams[$simpleFieldName];              
+        } 
+           
+        if (count($errorFields) > 0){ 
+            throw new CustomException($errorFields); 
+        } 
+
+        return $simpleFields;      
+    }
+}
