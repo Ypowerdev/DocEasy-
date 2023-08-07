e0bf60f256d4ab9e0b3716abc3b0241e31843835
Ypowerdev <117303538+Ypowerdev@users.noreply.github.com>
2023-08-07 18:31:54 +0300

    Update index.php

--- index.php
+++ index.php
@@ -4,18 +4,16 @@ session_start();
 
 define('APPLICATION_PATH', __DIR__); 
 
-spl_autoload_register(function(string $className) { 
-    $className = str_replace("\\", DIRECTORY_SEPARATOR, $className);
-    require_once __DIR__ . DIRECTORY_SEPARATOR . $className . '.php'; 
-}); 
+include 'vendor/autoload.php';
 
 $route = $_GET['route'] ?? '';
-$routes = require __DIR__ . '/../doceasy1/routes.php';
+$routes = require __DIR__ . '/../doceasy.ru/routes.php';
 
 try{ 
-    (new App\Router\Router($routes))->callAction($route);
+  (new App\Router\Router($routes))->callAction($route);
 }catch (\App\Exception\RouteNotFound $exception){ 
-    (new App\Classes\Controllers\ErrorController())->notFoundAction($exception);    
+  echo 'Страница c роутом ' . $exception->getRoute() . ' не найдена'; 
+  http_response_code(404); 
 }
 
 
